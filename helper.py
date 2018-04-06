0ad057f09f630048df50fba22b8c3319870e3cec
Tiffany <Tiffany@Xiaotongs-MacBook-Air.local>
2018-04-05 21:26:21 -0400

    copied from house

---
+++ helper.py
@@ -0,0 +1,150 @@
+import math
+import sys
+
+import numpy as np
+import pandas as pd
+import tensorflow as tf
+from IPython import display
+from matplotlib import cm, gridspec
+from matplotlib import pyplot as plt
+from sklearn import metrics
+from tensorflow.python.data import Dataset
+
+tf.logging.set_verbosity(tf.logging.ERROR)
+
+
+class tf_basic_model:
+    def preprocess_features(data_frame):
+        selected_features = data_frame[
+            [
+
+            ]
+        ]
+        preprocess_features = selected_features.copy()
+        preprocess_features = preprocess_features.apply(
+            lambda x: x.fillna(x.value_counts().index[0]))
+
+        return preprocess_features
+
+    def preprocess_targets(data_frame):
+        output_targets = pd.DataFrame()
+        output_targets = data_frame[['Ca', 'P', 'pH', 'SOC', 'Sand']]
+        return output_targets
+
+    def construct_feature_columns(input_features):
+        return set([tf.feature_column.numeric_column(my_feature) for my_feature in feature_columns])
+
+    def my_input_fn(features, targets, batch_size=1, shuffle=False, num_epochs=None):
+        features = {key: np.array(value)
+                    for key, value in dict(features).items()}
+
+        ds = tf.data.Dataset.from_tensor_slices((features, targets))
+        ds = ds.batch(batch_size).repeat(num_epochs)
+
+        if shuffle:
+            ds = ds.shuffle(10000)
+
+        features, labels = ds.make_one_shot_iterator().get_next()
+        return features, labels
+
+    def train_nn_regression_model(
+            learning_rate,
+            steps,
+            batch_size,
+            hidden_units,
+            training_examples,
+            training_targets,
+            validation_examples,
+            validation_targets):
+
+        periods = 10
+        steps_per_period = steps / periods
+
+        my_optimizer = tf.train.GradientDescentOptimizer(
+            learning_rate=learning_rate)
+        my_optimizer = tf.contrib.estimator.clip_gradients_by_norm(
+            my_optimizer, 5.0)
+        dnn_regressor = tf.estimator.DNNRegressor(
+            feature_columns=tf_basic_model.construct_feature_columns(
+                training_examples),
+            hidden_units=hidden_units,
+            optimizer=my_optimizer,
+        )
+
+        def training_input_fn(): return tf_basic_model.my_input_fn(training_examples,
+                                                                   training_targets["SalePrice"],
+                                                                   batch_size=batch_size)
+
+        def predict_training_input_fn(): return tf_basic_model.my_input_fn(training_examples,
+                                                                           training_targets["SalePrice"],
+                                                                           num_epochs=1,
+                                                                           shuffle=False)
+
+        def predict_validation_input_fn(): return tf_basic_model.my_input_fn(validation_examples,
+                                                                             validation_targets["SalePrice"],
+                                                                             num_epochs=1,
+                                                                             shuffle=False)
+
+        print("Training model...")
+        print("RMSE (on training data):")
+        training_rmse = []
+        validation_rmse = []
+        for period in range(0, periods):
+            dnn_regressor.train(
+                input_fn=training_input_fn,
+                steps=steps_per_period
+            )
+            # Take a break and compute predictions.
+            training_predictions = dnn_regressor.predict(
+                input_fn=predict_training_input_fn)
+            training_predictions = np.array(
+                [item['predictions'][0] for item in training_predictions])
+
+            validation_predictions = dnn_regressor.predict(
+                input_fn=predict_validation_input_fn)
+            validation_predictions = np.array(
+                [item['predictions'][0] for item in validation_predictions])
+
+            # Compute training and validation loss.
+            training_root_mean_squared_error = math.sqrt(
+                metrics.mean_squared_error(training_predictions, training_targets))
+            validation_root_mean_squared_error = math.sqrt(
+                metrics.mean_squared_error(validation_predictions, validation_targets))
+            # Occasionally print the current loss.
+            print("  period %02d : %0.2f" %
+                  (period, training_root_mean_squared_error))
+            # Add the loss metrics from this period to our list.
+            training_rmse.append(training_root_mean_squared_error)
+            validation_rmse.append(validation_root_mean_squared_error)
+        print("Model training finished.")
+
+        # Output a graph of loss metrics over periods.
+        plt.ylabel("RMSE")
+        plt.xlabel("Periods")
+        plt.title("Root Mean Squared Error vs. Periods")
+        plt.tight_layout()
+        plt.plot(training_rmse, label="training")
+        plt.plot(validation_rmse, label="validation")
+        plt.legend()
+
+        print("Final RMSE (on training data):   %0.2f" %
+              training_root_mean_squared_error)
+        print("Final RMSE (on validation data): %0.2f" %
+              validation_root_mean_squared_error)
+
+        return dnn_regressor
+
+    def get_input_fn(data_set, num_epochs=None, shuffle=True):
+        return tf.estimator.inputs.pandas_input_fn(x=pd.DataFrame({k: data_set[k].values for k in data_set.columns}), y=None, num_epochs=num_epochs, shuffle=shuffle)
+
+    def submit_prediction(model, testing_examples, testing_targets):
+        def predict_testing_input_fn(): return tf_basic_model.my_input_fn(
+            testing_examples, testing_targets['SalePrice'], num_epochs=1, shuffle=False)
+        submission = pd.DataFrame()
+        submission['Id'] = testing_examples['Id']
+        predictions = model.predict(input_fn=predict_testing_input_fn)
+        predictions = np.array([item['predictions'][0]
+                                for item in predictions])
+        submission['SalePrice'] = predictions
+        submission.head()
+        submission.to_csv('./data/submission.csv', index=False)
---
+++ run.py
@@ -0,0 +1,48 @@
+import math
+import sys
+
+import numpy as np
+import pandas as pd
+from IPython import display
+
+from helper import tf_basic_model
+
+pd.options.display.max_rows = 100
+pd.options.display.max_columns = 200
+pd.options.display.float_format = '{:.1f}'.format
+
+example_data_frame = pd.read_csv('./data/training.csv', sep=",")
+testing_data_frame = pd.read_csv('./data/sorted_test.csv', sep=",")
+example_data_frame = example_data_frame.reindex(
+    np.random.permutation(example_data_frame.index))
+
+display.display(example_data_frame.describe())
+
+
+sys.exit(0)
+
+
+training_dataframe = example_data_frame.head(958)
+validation_dataframe = example_data_frame.tail(200)
+
+
+training_examples = tf_basic_model.preprocess_features(training_dataframe)
+training_targets = tf_basic_model.preprocess_targets(training_dataframe)
+
+validation_examples = tf_basic_model.preprocess_features(validation_dataframe)
+validation_targets = tf_basic_model.preprocess_targets(validation_dataframe)
+
+testing_examples = tf_basic_model.preprocess_features(testing_data_frame)
+testing_targets = tf_basic_model.preprocess_targets(testing_data_frame)
+
+
+dnn_regressor = tf_basic_model.train_nn_regression_model(learning_rate=0.001,
+                                                            steps=2000,
+                                                            batch_size=100,
+                                                            hidden_units=[10, 10],
+                                                            training_examples=training_examples,
+                                                            training_targets=training_targets,
+                                                            validation_examples=validation_examples,
+                                                            validation_targets=validation_targets)
+
+prediction = tf_basic_model.submit_prediction(dnn_regressor, testing_examples, testing_targets)
